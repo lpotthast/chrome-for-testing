02c7b4ef46dcf53d3c2cfc19ffb8eb426fcd0816
Lukas Potthast <privat@lukas-potthast.de>
2025-01-10 10:48:56 +0100

    Add Display impl for Channel

--- src/api/channel.rs
+++ src/api/channel.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use std::fmt::Display;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
 pub enum Channel {
@@ -6,4 +7,24 @@ pub enum Channel {
     Beta,
     Dev,
     Canary,
+}
+
+impl Display for Channel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assertr::prelude::*;
+
+    #[test]
+    fn display_impl_formats_channels_as_expected(){
+        assert_that(format!("{}", Channel::Stable)).is_equal_to("Stable");
+        assert_that(format!("{}", Channel::Beta)).is_equal_to("Beta");
+        assert_that(format!("{}", Channel::Dev)).is_equal_to("Dev");
+        assert_that(format!("{}", Channel::Canary)).is_equal_to("Canary");
+    }
 }
